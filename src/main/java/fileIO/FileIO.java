ea9a25d3811e6789204b4c7278bc1bc3516ae7a6
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-02-22 23:35:14 +0100

    reorder

--- src/main/java/fileIO/FileIO.java
+++ src/main/java/fileIO/FileIO.java
@@ -33,13 +33,13 @@ public class FileIO implements IFileIO {
     @Override
     public void renameFile(File file, String appendix, Boolean shouldBeAdded) {
         Boolean result = false;
-        if(shouldBeAdded) {
-            result = file.renameTo(new File(file.getAbsolutePath() + appendix));
-        }else {
-            String renameTo = file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf(appendix));
-            result = file.renameTo(new File(renameTo));
-        }
         try {
+            if (shouldBeAdded) {
+                result = file.renameTo(new File(file.getAbsolutePath() + appendix));
+            } else {
+                String renameTo = file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf(appendix));
+                result = file.renameTo(new File(renameTo));
+            }
 
             if (!result) throw new Exception("Could not rename File: " + file.getAbsolutePath());
         } catch (Exception ex) {
